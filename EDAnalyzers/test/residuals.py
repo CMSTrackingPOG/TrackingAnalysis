b2e897e250ecb369efd01a2f9ae465a5a6bbd6bf
Kirill Skovpen <Kirill.Skovpen@cern.ch>
2019-12-18 14:26:21 +0100

    update

--- EDAnalyzers/test/residuals.py
+++ EDAnalyzers/test/residuals.py
@@ -3,18 +3,9 @@ import FWCore.ParameterSet.Config as cms
 readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring() 
 source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
-readFiles.extend( [  #before run introducing prescaling for jetTriggers (135445)
-#'/store/relval/CMSSW_10_6_0/RelValQCD_FlatPt_15_3000HS_13UP17/FEVTDEBUGHLT/PUpmx25ns_106X_mc2017_realistic_v3_ulhlt17hs_pmx-v1/10000/ECF91A89-E170-1E4A-827B-D7E06212D1A6.root'
-#'/store/relval/CMSSW_10_6_0/RelValMinBias_13/GEN-SIM-RECO/106X_mcRun2_asymptotic_v3-v1/10000/DD15408C-2088-4F47-822F-F1D5AED95767.root'
-#'/store/relval/CMSSW_10_6_0/ZeroBias/RECO/106X_dataRun2_relval_v9_RelVal_2016B-v1/10000/0BCB036A-1778-3C4E-8683-62185F9C8E95.root'
+readFiles.extend( [
 '/store/data/Run2016B/ZeroBias/AOD/07Aug17_ver1-v1/50000/D2F70B66-BD8B-E711-8761-E0071B7BC1A1.root'
 ]);
-
-# /ZeroBias/CMSSW_10_6_0-106X_dataRun2_relval_v9_RelVal_2016B-v1/RECO
-# /RelValMinBias_13/CMSSW_10_6_0-106X_mcRun2_asymptotic_v3-v1/GEN-SIM-RECO
-
-# /RelValTTbar_13/CMSSW_11_0_0_pre10-110X_mcRun2_asymptotic_v2-v1/GEN-SIM-RECO 
-# /RelValTTbar_13/CMSSW_11_0_0_pre10-PU25ns_110X_mcRun2_asymptotic_v2-v1/GEN-SIM-RECO
 
 secFiles.extend([ ]);
 
@@ -32,9 +23,12 @@ process.load('Configuration.StandardSequences.Reconstruction_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.EventContent.EventContent_cff')
 
+#process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+#from Configuration.AlCa.autoCond import autoCond
+#process.GlobalTag.globaltag = autoCond['com10_2011']
+
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-##process.GlobalTag.globaltag = '110X_mcRun2_asymptotic_v2'
-process.GlobalTag.globaltag = '80X_dataRun2_2016LegacyRepro_v4'
+process.GlobalTag.globaltag = 'FT_R_53_LV5::All'
 
 ##process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) ) # Data 
