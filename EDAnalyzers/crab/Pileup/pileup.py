84415bdad3632f385400a66d05efac42a42bf94c
Kirill Skovpen <Kirill.Skovpen@cern.ch>
2020-05-08 09:19:52 +0200

    update location of lumi

--- EDAnalyzers/crab/Pileup/pileup.py
+++ EDAnalyzers/crab/Pileup/pileup.py
@@ -8,8 +8,7 @@ from os.path import expanduser
 os.environ["PATH"] += os.pathsep + expanduser("~") + '/.local/bin'
 os.environ["PATH"] += os.pathsep + '/cvmfs/cms-bril.cern.ch/brilconda/bin'
 
-dlumi = '../'
-plumi = '/results/processedLumis.json'
+dlumi = 'processedLumi/'
 json = ['crab_JetHT_Run2017B__1', 'crab_JetHT_Run2017D__1', 'crab_JetHT_Run2017E__1', 'crab_JetHT_Run2017F__1', \
 'crab_ZeroBias_Run2017B__1', 'crab_ZeroBias_Run2017C__1', 'crab_ZeroBias_Run2017D__1', 'crab_ZeroBias_Run2017E__1', 'crab_ZeroBias_Run2017F__1']
 
@@ -23,7 +22,7 @@ for j in json:
     
     print j
     
-    os.system('brilcalc lumi --xing --normtag /cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_PHYSICS.json -i '+dlumi+j+plumi+' -o '+tmpdir+j+'.csv')
+    os.system('brilcalc lumi --xing --normtag /cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_PHYSICS.json -i '+dlumi+j+'.json -o '+tmpdir+j+'.csv')
     os.system('makePileupJSON.py --csvInput '+tmpdir+j+'.csv '+j+'.txt')
     os.system('pileupCalc.py -i '+dlumi+j+plumi+' --inputLumiJSON '+j+'.txt'+' --calcMode true --minBiasXsec '+xsec+' --maxPileupBin 100 --numPileupBins 100 '+j+'.root')
     
