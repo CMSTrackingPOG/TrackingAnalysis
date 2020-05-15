3366da38fe50ec8311d374ff1c097dfbaaee615f
Kirill Skovpen <Kirill.Skovpen@cern.ch>
2020-05-15 16:45:03 +0200

    add lumi calc script

---
+++ EDAnalyzers/crab/calcLumi.py
@@ -0,0 +1,10 @@
+#!/bin/env python
+
+import os
+
+ppath = [os.path.expanduser('~')+'/.local/bin', '/cvmfs/cms-bril.cern.ch/brilconda/bin'] 
+os.environ["PATH"] += os.pathsep + os.pathsep.join(ppath)
+
+fjson = 'JSON/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt'
+
+os.system('brilcalc lumi --normtag /cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_PHYSICS.json -u /fb  -i '+fjson+' -o lumi.csv')
