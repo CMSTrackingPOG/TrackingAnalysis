133d2f35a4073ae690d37768557580ccb753f1cf
Kirill Skovpen <Kirill.Skovpen@cern.ch>
2020-07-19 19:24:56 +0200

    update to latest

--- EDAnalyzers/test/respAndResoOnMC.py
+++ EDAnalyzers/test/respAndResoOnMC.py
@@ -4,45 +4,26 @@ readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring() 
 source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
 readFiles.extend( [
-#'file:/afs/cern.ch/work/k/kskovpen/GEN-SIM-DIGI-RAW-HLTDEBUG.root'
-'/store/relval/CMSSW_10_6_0/RelValQCD_FlatPt_15_3000HS_13UP17/FEVTDEBUGHLT/PUpmx25ns_106X_mc2017_realistic_v3_ulhlt17hs_pmx-v1/10000/ECF91A89-E170-1E4A-827B-D7E06212D1A6.root'
-#'/store/relval/CMSSW_10_6_0/ZeroBias/FEVTDEBUGHLT/103X_dataRun2_HLT_relval_v8_RelVal_2016B-v1/10000/F6017F7C-761F-1044-ABA7-80A710D7CFAD.root'
+'/store/mc/RunIISummer19UL17RECO/SingleNeutrino/AODSIM/FEVTDEBUG_106X_mc2017_realistic_v6-v2/20000/880C9813-73EA-574A-9208-A0DC4B177C13.root'
 ]);
 
-# /RelValTTbar_13/CMSSW_11_0_0_pre10-PU25ns_110X_mcRun2_asymptotic_v2-v1/GEN-SIM-RECO
-# /RelValTTbar_13/CMSSW_11_0_0_pre10-110X_mcRun2_asymptotic_v2_FastSim-v1/GEN-SIM-DIGI-RECO
-# /MinBias_TuneCUETP8M1_13TeV-pythia8/RunIIFall15DR76-PU25nsData2015v1Recodebug_76X_mcRun2_asymptotic_RunIIFall15DR76_v1-v1/GEN-SIM-RECODEBUG
-# /ZeroBias/Run2015D-PromptReco-v4/RECO
-
-# /RelValQCD_FlatPt_15_3000HS_13UP17/CMSSW_10_6_0-PUpmx25ns_106X_mc2017_realistic_v3_ulhlt17hs_pmx-v1/FEVTDEBUGHLT
-# /ZeroBias/CMSSW_10_6_0-103X_dataRun2_HLT_relval_v8_RelVal_2016B-v1/FEVTDEBUGHLT
-
-# go to https://twiki.cern.ch/twiki/bin/view/CMS/PdmVRelVals2019
-# run runTheMatrix.py command to get the info on the cmsDriver.py
-# runTheMatrix.py -n
-# runTheMatrix.py --what pileup -l 25202 --dryRun
-
-secFiles.extend( [ ]);
+secFiles.extend( [
+'/store/mc/RunIISummer19UL17DIGI/SingleNeutrino/GEN-SIM-DIGI-RAW/FEVTDEBUG_106X_mc2017_realistic_v6-v2/210002/19281869-E1AD-9C41-986F-7D87A5A94602.root'
+] );
 
 process = cms.Process("IpResolutions")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 10000
-# import of standard configurations
+
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load("Configuration.StandardSequences.MagneticField_cff")
-#process.load('Configuration.StandardSequences.RawToDigi_cff')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 
-#from SimGeneral.MixingModule.pixelDigitizer_cfi import *
-
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = '110X_mcRun2_asymptotic_v2'
-
-##process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-##process.GlobalTag.globaltag = 'SET_GLOBALTAG'
+process.GlobalTag.globaltag = '106X_mc2017_realistic_v6'
 
 #process.load("SimTracker.Configuration.SimTracker_cff")
 
