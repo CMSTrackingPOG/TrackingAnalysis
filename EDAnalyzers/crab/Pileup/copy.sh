ab06c0c6f160412adcfa97e7fa052c51a951e376
Kirill Skovpen <Kirill.Skovpen@cern.ch>
2022-03-15 06:19:12 +0100

    update

---
+++ EDAnalyzers/crab/Pileup/copy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp ../crab_JetHT_Run2017B_UL2017_1/results/processedLumis.json processedLumi/JetHT_Run2017B_UL2017.json
+cp ../crab_JetHT_Run2017C_UL2017_1/results/processedLumis.json processedLumi/JetHT_Run2017C_UL2017.json
+cp ../crab_JetHT_Run2017D_UL2017_1/results/processedLumis.json processedLumi/JetHT_Run2017D_UL2017.json
+cp ../crab_JetHT_Run2017E_UL2017_1/results/processedLumis.json processedLumi/JetHT_Run2017E_UL2017.json
+cp ../crab_JetHT_Run2017F_UL2017_1/results/processedLumis.json processedLumi/JetHT_Run2017F_UL2017.json
