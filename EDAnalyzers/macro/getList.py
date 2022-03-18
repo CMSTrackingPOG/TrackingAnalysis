227d640487b0bbe2f488b3293d661cc7fb03f78e
Kirill Skovpen <Kirill.Skovpen@cern.ch>
2022-03-18 13:07:06 +0100

    update

--- EDAnalyzers/macro/getList.py
+++ EDAnalyzers/macro/getList.py
@@ -1,77 +1,60 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import sys
 import json
 
-#ntupleProd = 'v20200408'
-ntupleProd = 'v20200423'
+ntupleProd = 'v20220314'
+year = 'UL17'
 
 sys.tracebacklimit = 0
 
 if __name__ == '__main__':
 
-    loc = 'srm://maite.iihe.ac.be:/pnfs/iihe/cms/store/user/kskovpen/Track/Ntuple/Track-'+ntupleProd+'/'
-
-#    fout = open("list.txt","w+")
+    lsc = 'xrdfs maite.iihe.ac.be ls ' 
+    
+    loc = '/pnfs/iihe/cms/store/user/kskovpen/Track/Ntuple/Track-'+ntupleProd+'/'
 
     files = {}
 
-    dlist = os.popen('gfal-ls '+loc).read().splitlines()
+    dlist = os.popen(lsc+loc).read().splitlines()
 
     for i in dlist:
 
-        ds = i
+        ds = i.split('/')[-1]
     
-        ds = ds.replace('/','')
-#        print ds
-        ds = ds.replace('-','')
-        ds = ds.replace('_','')
+        flist = os.popen(lsc+i).read().splitlines()
 
-#        files[ds] = []
-        
-#        fout.write(ds+' = [\n')
-
-        flist = os.popen('gfal-ls '+loc+i).read().splitlines()
-    
         for j in range(len(flist)):
-            
-            dss = flist[j]
-            dss = dss.replace('/','')
-            dss = dss.replace('-','')
-            dss = dss.replace('_','')
- 
+
+            if year not in flist[j] and year.replace('17', '2017') not in flist[j]: continue
+
+            dss = flist[j].split('/')[-1]
+
             dname = ds+'_'+dss
-            print dname
-            
+            print(dname)
+
             # do not run on samples with BS constraint
             if 'withBS' in dname: continue
             
             files[ds+'_'+dss] = []
-            
-            flistt = os.popen('gfal-ls '+loc+i+'/'+flist[j]).read().splitlines()
+
+            flistt = os.popen(lsc+flist[j]).read().splitlines()
 
             for jj in range(len(flistt)):
         
-                flisttt = os.popen('gfal-ls '+loc+i+'/'+flist[j]+'/'+flistt[jj]).read().splitlines()
+                flisttt = os.popen(lsc+flistt[jj]).read().splitlines()
                 
                 for jjj in range(len(flisttt)):
                     
-                    flistttt = os.popen('gfal-ls '+loc+i+'/'+flist[j]+'/'+flistt[jj]+'/'+flisttt[jjj]).read().splitlines()
+                    flistttt = os.popen(lsc+flisttt[jjj]).read().splitlines()
                     
                     for jjjj in range(len(flistttt)):
                         
                         if 'log' not in flistttt[jjjj]:
                 
-#                            fout.write("'"+loc+i+'/'+flist[j]+'/'+flistt[jj]+'/'+flisttt[jjj]+'/'+flistttt[jjjj]+"'")
-                            floc = loc+i+'/'+flist[j]+'/'+flistt[jj]+'/'+flisttt[jjj]+'/'+flistttt[jjjj]
-                            floc = floc.replace('srm','root')
+                            floc = 'root://maite.iihe.ac.be/'+flistttt[jjjj]
                             files[ds+'_'+dss].append(floc)
 
-#                        if jjjj != len(flistttt)-1:
-#                            fout.write(',\n')
-#                        else:
-#                            fout.write(']\n\n')
-
-    with open("list.json", "w") as write_file:
+    with open("samples"+year+".json", "w") as write_file:
         json.dump(files, write_file, indent=2)
